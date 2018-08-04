982022ca6c4df1be46f89d7514767bc9f7c5c8c6
hisham <hisham@ceylonit.com>
2018-08-04 09:19:31 +0530

    login and register

--- app/Http/Models/Content.php
+++ app/Http/Models/Content.php
@@ -11,5 +11,5 @@ class Content extends Model
     /**
      * @var array
      */
-    protected $fillable = ['name', 'access_level_id', 'type', 'content', 'lat', 'long', 'user_id', 'is_deleted','user_ip'];
+    protected $fillable = ['name', 'access_level_id', 'type', 'content', 'lat', 'long', 'user_id', 'is_deleted','user_ip', 'url'];
 }
