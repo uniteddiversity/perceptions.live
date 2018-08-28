dd05ad25226929594535830e83345e94aaf2ee6a
hisham <hisham@ceylonit.com>
2018-08-28 08:52:57 +0530

    group sharing fixes.

--- resources/views/layouts/shared_content_layout.blade.php
+++ resources/views/layouts/shared_content_layout.blade.php
@@ -43,7 +43,11 @@
 <!-- plugins:js -->
 
     {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>--}}
-
+<style>
+    #featureModal .modal-dialog{
+        width: 950px;
+    }
+</style>
 <script src="/assets/js/jquery-2.1.4.min.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
 <script src="/assets/js/typeahead.bundle.min.js"></script>
