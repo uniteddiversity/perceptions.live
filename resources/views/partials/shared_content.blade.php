0ddeaae80f2f4071fbb56c4836be14625560759b
hisham <hisham@ceylonit.com>
2018-08-28 23:25:36 +0530

    group sharing fixes.

--- resources/views/partials/shared_content.blade.php
+++ resources/views/partials/shared_content.blade.php
@@ -6,7 +6,7 @@
     <div style="float: left;max-width: 320px;">
         @include('partials.shared_left-side-bar')
     </div>
-    <div class="col-md-9 no-float col-sm-8half" id="map" style="height: 350px;float: left;width: auto;min-width: 450px;">Loading..</div>
+    <div class="col-md-9 no-float col-sm-8half" id="map" style="height: 350px;float: left;width: auto;min-width: 550px;">Loading..</div>
     {{--<div id="loading">--}}
         {{--<div class="loading-indicator">--}}
             {{--<div class="progress progress-striped active">--}}
