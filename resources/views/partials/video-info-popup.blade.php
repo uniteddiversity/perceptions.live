bac426618f6d06a3f3ebf87e070fa4df9ff974e8
jordan <jordan@perceptiontravel.tv>
2018-09-30 21:44:44 +0800

    match video box to leaflet

--- resources/views/partials/video-info-popup.blade.php
+++ resources/views/partials/video-info-popup.blade.php
@@ -91,21 +91,43 @@
 </div>
 */ ?>
 
-<iframe width="200" height="100" src="<?php echo str_replace( 'watch?v=', 'embed/',$info['url']) ?>" frameborder="0" allowfullscreen></iframe>
-<div class="row" style="display: block;margin: 5px;">
-        <div style="font-size: 16px;margin-top: 6px;margin-bottom: 6px;">{{$info['title']}}
-            <span style="padding-bottom: 6px;float: right;">
-                <?php foreach($info->gciTags as $tag){
-                    if(isset($tag->tag) && isset($tag->tag->tag))
-//                    echo '<br/> tag '.$tag->tag->tag;
-                    echo '<span style="background-color: '.$tag->tag->tag_color.'" class="dot-small"></span>';
-                }
-                ?>
+<div class="places s2">
+    <div class="placedetails">
+        <span class="pull-left" onclick="navigateOnMap('<?php echo $info['lat'] ?>','<?php echo $info['long'] ?>');" style="cursor: pointer;"><i class="flaticon-pin"></i> <?php echo $info['location'] ?></span>
+        <span class="pull-right"><i class="flaticon-avatar"></i> <?php foreach($info->videoProducer as $key => $users){ if(isset($info->videoProducer[$key])){ echo '<span class="inactive_link" onclick="openProfile(\''. $info->videoProducer[$key]->user->id .'\')">@'.$info->videoProducer[$key]->user->display_name.'</span>'; break; } }?></span>
+    </div>
+    <div class="placethumb">
+        <img src="https://img.youtube.com/vi/<?php echo $video_id ?>/mqdefault.jpg">
+        <?php /*<iframe frameborder="0" showinfo="0" controls="0" autohide="1" style="width: 100%;" src="<?php echo str_replace( 'watch?v=', 'embed/',$info['url']) ?>" frameborder="0" allowfullscreen></iframe> */ ?>
+        <div class="watchicon2" onclick="openVideo('<?php echo $info['id'] ?>')""> <a href="#" title=""><img src="/assets/findgo/images/play3.png" alt=""> </a>
+    </div>
+</div>
+<div class="boxplaces">
+    <div class="placeinfos">
+
+        <h3><a href="#" title="" onclick="openVideo('<?php echo $info['id'] ?>')"><?php echo $info['title'] ?></a></h3>
+        <span style="padding-bottom: 6px; float: right;">
+                    <?php
+
+            $tags = explode(',', $info['tag_colors']);
+            foreach($tags as $tag){
+                $tag_name_n_color = explode('-', $tag);
+                $tag_id = isset($tag_name_n_color[1])? $tag_name_n_color[1] : '0';
+                echo '<span onclick="searchByTag(\''.$tag_id.'\')" style="background-color: '.$tag_name_n_color[0].'" class="dot-small"></span>';
+            } ?>
                 </span>
-        </div>
-        <div><i class="flaticon-pin"> {{$info['location']}}</i></div>
-                <div><i class="flaticon-avatar"></i> <?php foreach($info->videoProducer as $key => $users){ if(isset($info->videoProducer[$key])){ echo '<span class="inactive_link" onclick="openProfile(\''. $info->videoProducer[$key]->user->id .'\')">@'.$info->videoProducer[$key]->user->display_name.'</span>'; break; } }?></div>
+        <span style="padding-bottom: 6px;"><small><i class="fa fa-calendar"></i> <em><?php echo date('d F Y',strtotime($info['created_at'])) ?></em></small></span>
+    </div>
+</div><div class="placeinfosdesc">
+    <?php if(!empty($info['trim_description'])){ ?>
+    <span style="font-size: .8em;"><?php echo $info['trim_description'] ?> [...]</span>
+    <?php } ?>
 
-        <div><a href="#" onclick="openVideo('<?php echo $info['id'] ?>')"><i class="flaticon-eye"> Watch / View More Info</i></a></div>
+    <ul class="listmetas">
+        <li><i class="fa fa-exchange"></i> EXCHANGE <?php echo $info['exchange'] ?></li>
+        <li><a href="#" title=""><i class="fa fa-tag"></i><?php echo $info['primary_subject_tag'] ?></a></li>
+    </ul>
+</div>
 
+</div>
 </div>
