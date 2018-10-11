47a3c618359e83c59a46a62030bc8c728ac1475d
hisham <hisham@ceylonit.com>
2018-10-11 07:44:01 +0530

    new template works for CP

--- resources/views/partials/group-admin-left-sidebar.blade.php
+++ resources/views/partials/group-admin-left-sidebar.blade.php
@@ -20,13 +20,13 @@
                             <span class="pcoded-mcaret"></span>
                         </a>
                     </li>
-                    <li class="<?php if(in_array(Request::segment(3), array('content-list-open'))){ echo 'active'; } ?>">
-                        <a class="nav-link" href="/user/group-admin/content-list-open">
-                            <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
-                            <span class="pcoded-mtext" data-i18n="nav.dash.default">Open Videos</span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
+                    {{--<li class="<?php if(in_array(Request::segment(3), array('content-list-open'))){ echo 'active'; } ?>">--}}
+                        {{--<a class="nav-link" href="/user/group-admin/content-list-open">--}}
+                            {{--<span class="pcoded-micon"><i class="ti-angle-right"></i></span>--}}
+                            {{--<span class="pcoded-mtext" data-i18n="nav.dash.default">Open Videos</span>--}}
+                            {{--<span class="pcoded-mcaret"></span>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
                     <li class="<?php if(in_array(Request::segment(3), array('content-list'))){ echo 'active'; } ?>">
                         <a class="nav-link" href="/user/group-admin/content-list">
                             <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
@@ -139,120 +139,6 @@
 </nav>
 
 
-<?php /*
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-        <li class="nav-item nav-profile">
-            <div class="nav-link">
-                <div class="user-wrapper">
-                    <div class="profile-image">
-                    <?php $user_img = Auth::user()->image; ?>
-                    <img src="<?php if(isset($user_img[0])){ echo '/storage/'.$user_img[0]->url; }else{ ?>/assets/img/face1.png<?php } ?>" alt="profile image">
-                    </div>
-                    <div class="text-wrapper">
-                        <p class="profile-name">{{Auth::user()->email}}</p>
-                        <div>
-                            <small class="designation text-muted">{{Auth::user()->role->name}}</small>
-                            <span class="status-indicator online"></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a href="/" class="nav-link" aria-expanded="false" aria-controls="">
-                <i class="menu-icon mdi mdi-face-profile"></i>
-                <span class="menu-title">Home/Map</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#contnet-sub" aria-expanded="false" aria-controls="contnet-sub">
-                <i class="menu-icon mdi mdi-table"></i>
-                <span class="menu-title">Video</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="contnet-sub">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/content-list-open"> Open Videos </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/content-list"> All Videos </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/content-add"> Add Video </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/sorting-tag-add"> Tags </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/location-list"> Location List </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="menu-icon mdi mdi-restart"></i>
-                <span class="menu-title">User Manage</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/user-list"> List Users </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/user-add"> Create User </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/user-group-add"> Create Group </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/user-to-group-add"> Assign User to Group </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/group-list"> List Groups </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-
-        <li class="nav-item">
-            <a href="/user/user-profile" class="nav-link" aria-expanded="false" aria-controls="">
-                <i class="menu-icon mdi mdi-face-profile"></i>
-                <span class="menu-title">Profile Setting</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#share" aria-expanded="false" aria-controls="share">
-                <i class="menu-icon mdi mdi-restart"></i>
-                <span class="menu-title">Shearing</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="share">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/map-generate-list"> Sheared List </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user/admin/map-generate"> Map Generate </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-
-
-        <li class="nav-item">
-            <a href="/user/logout" class="nav-link" aria-expanded="false" aria-controls="">
-                <i class="menu-icon mdi mdi-logout"></i>
-                <span class="menu-title">Log Out</span>
-            </a>
-        </li>
-    </ul>
-</nav>
- */ ?>
 <?php /*
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
