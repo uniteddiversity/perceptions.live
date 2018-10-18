423dbefee13397835954c932aeb583e542977bd3
hisham <hisham@ceylonit.com>
2018-10-18 05:55:46 +0530

    user side bar

--- resources/views/partials/user-left-sidebar.blade.php
+++ resources/views/partials/user-left-sidebar.blade.php
@@ -1,4 +1,40 @@
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
+<nav class="pcoded-navbar" >
+    <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
+    <div class="pcoded-inner-navbar main-menu">
+
+        <div class="pcoded-navigatio-lavel" data-i18n="nav.category.navigation" >Navigation</div>
+
+
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu <?php if(in_array(Request::segment(3), array('content-add','content-list-open','content-list','location-list','sorting-tag-add'))){ echo 'active pcoded-trigger'; } ?> pcoded-trigger">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="ti-video-camera"></i></span>
+                    <span class="pcoded-mtext" data-i18n="nav.dash.main">Video</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class="<?php if(in_array(Request::segment(3), array('content-add'))){ echo 'active'; } ?>">
+                        <a class="nav-link" href="/user/content-add">
+                            <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
+                            <span class="pcoded-mtext" data-i18n="nav.dash.default">Add Video</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu">
+                <a href="/user/logout">
+                    <span class="pcoded-micon"><i class="ti-layout-sidebar-left"></i></span>
+                    <span class="pcoded-mtext" data-i18n="nav.page_layout.main">Log Out</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<?php /* <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item nav-profile">
             <div class="nav-link">
@@ -62,3 +98,4 @@
         </li>
     </ul>
 </nav>
+ */ ?>
