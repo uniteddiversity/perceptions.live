842c51bc25c1768304975a582226c284c6291338
jordan <jordan@perceptiontravel.tv>
2018-10-20 20:05:41 -0700

    tooltip

--- resources/views/partials/home-left-side-bar.blade.php
+++ resources/views/partials/home-left-side-bar.blade.php
@@ -12,9 +12,9 @@
             @endforeach
         </select>
     </div>
-    <div style="font-family: ralewaybold; font-size: 1em; width: 100%; padding-left: 14px; float: left;">
-        <a class="tooltip2" href="#"> <span title="What are Greater Community Intentions?" style="margin-top:-10px; background: rebeccapurple; background: -webkit-linear-gradient(left, orange , yellow, green, cyan, blue, violet); background: -o-linear-gradient(right, orange, yellow, green, cyan, blue, violet); background: -moz-linear-gradient(right, orange, yellow, green, cyan, blue, violet); background: linear-gradient(to right, orange , yellow, green, cyan, blue, violet);" class="dot">
-           ?<span class="aboutgci">Use the <em>Greater Community Intentions</em> dots to sort through the different styles of community gathering around the world.</span></a>
+    <div style="font-family: ralewaybold; font-color: white; font-size: 1em; width: 100%; padding-left: 14px; float: left;">
+        <span title="What are Greater Community Intentions?" style="margin-top:-10px; background: rebeccapurple; background: -webkit-linear-gradient(left, orange , yellow, green, cyan, blue, violet); background: -o-linear-gradient(right, orange, yellow, green, cyan, blue, violet); background: -moz-linear-gradient(right, orange, yellow, green, cyan, blue, violet); background: linear-gradient(to right, orange , yellow, green, cyan, blue, violet);" class="dot">
+           <a class="tooltip2" href="#"> ? <span class="aboutgci">Use the <em>Greater Community Intentions</em> dots to sort through the different styles of community gathering around the world.</span></a>
         </span>
         <?php
         foreach($gci_tags as $tag){
