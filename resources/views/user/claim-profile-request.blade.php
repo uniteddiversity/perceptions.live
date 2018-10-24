70463a2afd1ec43ecc3900fc03d32944c3bb0279
jordan <jordan@perceptiontravel.tv>
2018-10-23 20:47:53 -0700

    claim profile form

--- resources/views/user/claim-profile-request.blade.php
+++ resources/views/user/claim-profile-request.blade.php
@@ -36,9 +36,9 @@ $data['proof_images'] = array();
                         <form action="/claim-profile-post" method="post" enctype='multipart/form-data'>
                             <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
                             <div class="form-group">
-                                <label for="display_name">Display Name</label>
+                                <label for="display_name">Select the display name you've seen</label>
                                 <select class="form-control" id="display_name_for_claim" name="display_name">
-                                    <option value="">Select Display Name</option>
+                                    <option value="">select display name</option>
                                     @foreach($user_list as $user)
                                         <option value="{{$user->id}}" >{{$user->display_name}}</option>
                                     @endforeach
@@ -46,14 +46,14 @@ $data['proof_images'] = array();
                             </div>
 
                             <div class="form-group">
-                                <label for="video_profile">Select video profiles for selected username</label>
+                                <label for="video_profile">Confirm the videos you've seen yourself in: </label>
                                 <select class="form-control multi-select2" multiple id="claim_video_profile" name="claim_video_profile[]">
 
                                 </select>
                             </div>
 
                             <div class="form-group">
-                                <label for="confirm_selected_content">I confirm that this is my involvement in these videos:</label>
+                                <label for="confirm_selected_content">I confirm that this is my involvement, appearance, and/or content in these videos: </label>
                                 <input type="checkbox" name="confirm_selected_content" id="confirm_selected_content" value="1" />
                             </div>
 
@@ -62,12 +62,15 @@ $data['proof_images'] = array();
                                 <textarea class="form-control" placeholder="Additional comments, (lost videos, requests, etc)" rows="5" name="additional_comments">{{ old('additional_comments') }}</textarea>
                             </div>
                             <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="text" class="form-control" aria-describedby="nameHelp" name="email" id="email" placeholder="Email" value="{{ old('email') }}">
+                                <label for="email">Your E-mail Address</label>
+                                <input type="text" class="form-control" name="email" id="email" placeholder="Email">
                             </div>
 
                             <div class="form-group">
-                                <label for="accept_tos">Proof of Identity in existing video</label>
+                                <label for="accept_tos">Relevant Proof of Identity</label>
+                                <div class="formdesctext">
+                                    <em>Please upload some evidence that you are who you claim to be (i.e. selfie or personal document) </em>
+                                </div>
                                 <input class="form-control" type="file" multiple name="proof_of_work[]" />
 
                                 <?php foreach($data['proof_images'] as $img){ ?>
@@ -76,7 +79,7 @@ $data['proof_images'] = array();
                             </div>
 
                             <div class="form-group">
-                                <label for="password">Terms of Service:</label>
+                                <label for="password">I have read and understand PRCPTION Travel's Terms of Service: </label>
                                 <input type="checkbox" name="accept_tos" id="accept_tos" value="1" />
                             </div>
 
@@ -93,7 +96,7 @@ $data['proof_images'] = array();
     </div>
 <style>
 .formdesctext {
-font-size: 12px;
+font-size: 14px;
     font-style: italic;
 font-family: questrial;
 line-height: 1.1em;
@@ -102,7 +105,7 @@ line-height: 1.1em;
 
 .formtextbottom { padding-top: 20px;}
 
-.claimprofile { width: 50%; margin: auto; }
+.claimprofile { width: 50%; margin: auto; padding-top: 40px; }
 </style>
 
 @endsection
