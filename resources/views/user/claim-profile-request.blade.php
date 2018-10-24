165463765c2aa81acef024fdeca58f5cd2cfa1df
jordan <jordan@perceptiontravel.tv>
2018-10-23 21:08:41 -0700

    claim profile

--- resources/views/user/claim-profile-request.blade.php
+++ resources/views/user/claim-profile-request.blade.php
@@ -14,10 +14,11 @@ $data['proof_images'] = array();
                  <a href="/"><img src="/assets/findgo/images/live-perceptions-logo.png" width="600" height="122"></a>
                 </div>
                 <div class="claimprofile">
-                <h4 align=center class="card-title">Claim A User Profile</h4>
-                <div class="formdesctext" align="center">
+                <h3 align=center class="card-title">Claim A Username</h3>
+                <div class="pagedesc" align="center">
                 <em>The PRCPTIONS.live platform creates 'shadow profiles' of users featured in a piece of but not actually registered for the site. If you happen to stumble upon a shadow profile that is you, please fill out this form to claim it as your own.</em>
                 </div>
+                    <hr>
                     <div class="table-responsive">
                     @if ($errors->any())
                         <div class="alert alert-danger">
@@ -51,10 +52,11 @@ $data['proof_images'] = array();
 
                                 </select>
                             </div>
-
+<hr>
                             <div class="form-group">
-                                <label for="confirm_selected_content">I confirm that this is my involvement, appearance, and/or content in these videos: </label>
                                 <input type="checkbox" name="confirm_selected_content" id="confirm_selected_content" value="1" />
+                                <label for="confirm_selected_content"> I confirm that this is my involvement, appearance, and/or content in these videos.</label>
+
                             </div>
 
                             <div class="form-group">
@@ -70,20 +72,18 @@ $data['proof_images'] = array();
                             </div>
 
                             <div class="form-group">
-                                <label for="additional_comments">Additional information, comments, questions, or concerns: </label>
-                                <textarea class="form-control" placeholder="Additional comments, (lost videos, requests, etc)" rows="5" name="additional_comments">{{ old('additional_comments') }}</textarea>
+                                <label for="additional_comments">Additional Information, Comments or Requests</label>
+                                <textarea class="form-control" placeholder="(your name, location, organization, participation)" rows="5" name="additional_comments">{{ old('additional_comments') }}</textarea>
                             </div>
                                 <hr style="padding: 10px;">
                             <div class="form-group">
                                 <label for="email">Your E-mail Address</label>
-                                <input type="text" class="form-control" name="email" id="email" placeholder="Email">
+                                <input type="text" name="email" id="email">
                             </div>
 
-
-
                             <div class="form-group">
-                                <label for="password">I have read and understand PRCPTION.LIVE's <a href="https://perceptiontravel.tv/terms-of-service/">Terms of Service</a>: </label>
                                 <input type="checkbox" name="accept_tos" id="accept_tos" value="1" />
+                                <label for="password"> I have read and understand PRCPTION Travel's <a href="https://perceptiontravel.tv/terms-of-service/">Terms of Service</a>.</label>
                             </div>
 
 
@@ -92,6 +92,7 @@ $data['proof_images'] = array();
                         <div class"formtextbottom">
                             <a href="/"><- Return to PRCPTIONS.LIVE</a>
                         </div>
+                    <div class="footer">&copy; 2018 PRCPTION Travel, Inc.</div>
                     </div>
                 </div>
             </div>
@@ -103,7 +104,7 @@ $data['proof_images'] = array();
         font-style: italic;
         font-family: questrial;
         line-height: 1.6em;
-        padding-bottom: 20px;
+        padding-bottom: 40px;
     }
 
     .formdesctext {
@@ -114,7 +115,11 @@ $data['proof_images'] = array();
          padding-bottom: 10px;
      }
 
-.formtextbottom { padding-top: 20px;}
+    .footer { width: 100%; margin: auto; text-align: center; font-size: 12px; color: slategray; }
+
+.formtextbottom a {
+    padding-top: 20px;
+}
 
 .claimprofile { width: 50%; margin: auto; padding-top: 40px; }
 </style>
