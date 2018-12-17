36f4f2ac54acace35ef13e382875b63fdd0ae455
kusan <kushanruchira@gmail.com>
2018-12-17 09:29:24 +0530

    edit videos feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -575,8 +575,9 @@ class UserController extends Controller
     {
         $video_editor_url = env("VIDEO_EDITOR_URL", "");
         $user_id = (!isset(Auth::user()->id))? 0 : Auth::user()->id;
-        $token_key = md5($user_id + time() + rand(0,10));
+        $token_key = md5(rand(0,1000) + $user_id + time() + rand(0,1000));
 
+        $userEditVideos->where('user_id', $user_id)->update( array('is_deleted' => '1') );
         //delete other tokens of that user
         $temp_data = $userEditVideos->create(array('user_id' => $user_id, 'token' => $token_key,
             'info' => json_encode(array('test' => 'test video')), 'is_deleted' => '0'));
