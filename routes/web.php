360b3eb5a1425150a7ea48eb696d8c1f12827def
kusan <kusan@ceylonit.com>
2019-09-28 09:32:28 +0530

    bug fixes

--- routes/web.php
+++ routes/web.php
@@ -185,7 +185,7 @@ Route::group(['prefix' => 'user', 'middleware' => ['auth', 'web', 'groupadmin']]
 //    Route::get('/group-admin/user-add', '\App\Controllers\User\GroupAdminController@userAdd');
     Route::get('/group-admin/user-edit/{_user_id}', '\App\Controllers\User\GroupAdminController@adminUserEdit');
     Route::get('/group-admin/video-edit/{_content_id}', '\App\Controllers\User\GroupAdminController@contentEdit');
-    Route::post('/admin/post-upload-video', '\App\Controllers\User\AdminController@postUploadVideo');
+    Route::post('/group-admin/post-upload-video', '\App\Controllers\User\AdminController@postUploadVideo');
     Route::post('/group-admin/post-user-add', '\App\Controllers\User\GroupAdminController@adminUserAdd');
     Route::get('/group-admin/content-list', '\App\Controllers\User\GroupAdminController@contentList');
     Route::get('/group-admin/content-list-group', '\App\Controllers\User\GroupAdminController@groupContentListNew');
