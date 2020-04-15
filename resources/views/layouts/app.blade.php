f71ee21fc2652a614ce00914e0e287bdb33e86da
kushan <kusan@ceylonit.com>
2020-04-15 17:13:20 +0530

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -144,7 +144,12 @@
 
         // start intro.js
         $(document).ready(function() {
-            intro.start();
+            let intro_exited = getCookie('intro_exited');
+
+            let home_url = window.location.pathname;
+            if(home_url == '/' && intro_exited == null){
+                intro.start();
+            }
         });
 
         // tour button
@@ -153,6 +158,34 @@
             intro.start("#step2");
         };
 
+        intro.onchange(function () {
+            // console.log(this);
+            // document.cookie = "intro_exited=true; user-id=0; date="+Date.now();
+        });
+
+        intro.onexit(function () {
+            document.cookie = "intro_exited=true; path=/";
+        });
+
+        function getCookie(name) {
+            // Split cookie string and get all individual name=value pairs in an array
+            var cookieArr = document.cookie.split(";");
+
+            // Loop through the array elements
+            for(var i = 0; i < cookieArr.length; i++) {
+                var cookiePair = cookieArr[i].split("=");
+
+                /* Removing whitespace at the beginning of the cookie name
+                and compare it with the given string */
+                if(name == cookiePair[0].trim()) {
+                    // Decode the cookie value and return
+                    return decodeURIComponent(cookiePair[1]);
+                }
+            }
+
+            // Return null if not found
+            return null;
+        }
     </script>
 
 
