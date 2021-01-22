2fd35c449b88ea2706f676fad6aa53920cfe524c
KUSAN <kusan@efuturesworld.com>
2021-01-22 20:41:31 +0530

    some changes on install script

--- public/installer/env_editor.php
+++ public/installer/env_editor.php
@@ -7,14 +7,12 @@ if(isset($_POST['submit_all'])){
 }
 
 function moveIcons(){
-
-    dd($_FILES);
-
     $target_dir = "/public/assets/img/";
     $target_dir = "./";
-    $target_file = $target_dir . 'moved_image.png';
+    $logo = $target_dir . 'moved_image.png';
+    $logo_small = $target_dir . 'moved_image_small.png';
     $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $imageFileType = strtolower(pathinfo($logo,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
@@ -50,17 +48,19 @@ function moveIcons(){
 // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
     } else {
         if(isset($_FILES["site_logo"])){
-            die('www');
-            if (move_uploaded_file($_FILES["site_logo"]["tmp_name"], $target_file)) {
+            if (move_uploaded_file($_FILES["site_logo"]["tmp_name"], $logo_small)) {
                 echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
             } else {
+                die('SORRY');
                 echo "Sorry, there was an error uploading your file.";
             }
         }
 
+        if(isset($_FILES["site_logo"])){
+
+        }
     }
 
 }
